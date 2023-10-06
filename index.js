23c383aec7c10e231e1e6104334f69503b4b9a46
Renan Kozuka Micali <renanmicali@hotmail.com>
2023-10-06 11:54:30 +1100

    function lab complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    const street = 42
+    const distance = Math.abs(block - street)
+    return distance
+}
+
+
+
+function distanceFromHqInFeet(feet) {
+    return distanceFromHqInBlocks(feet) * 264;
+         
+}
+
+const distanceTravelledInFeet = (start,destination) => {
+    return Math.abs((start - destination)*264)
+}
+
+const calculatesFarePrice = (start,destination) => {
+    const distance = Math.abs((start - destination)*264)
+    if (distance <= 400) {
+        return 0
+    } else if ( distance >= 400 && distance <= 2000){
+        return (distance - 400)* 0.02
+    } else if (distance >=2000 && distance <= 2500){
+        return 25
+    } else {
+        return `cannot travel that far`
+    }
+}
